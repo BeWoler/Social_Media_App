7f97eb304fac4eadad541ad8a5af82941976fb57
BeWoler <bewoler.del@gmail.com>
2023-11-20 20:13:50 +0300

    added icons & 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,7 @@
 export default function NotFound() {
   return (
-    <div className="flex text-6xl mt-auto h-screen items-center">404 Not Found :o</div>
+    <div className="flex text-6xl mt-auto h-screen items-center">
+      404 Not Found :o
+    </div>
   );
 }
